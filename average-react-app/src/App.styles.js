3a54cb1715be68671acc69a3a0981a01c45de76a
JavaScript Joe <jjprevite@gmail.com>
2019-07-21 11:13:25 -0700

    Use the Effect Hook in Function Components
    In this lesson, we used the Effect Hook useEffect to  see how it differed from useState. We also noted ho w they can be used together. Our example added a click event listener to our main app and looked at the elements within the click path to help us determine whether or not we should close our menu.

--- average-react-app/src/App.styles.js
+++ average-react-app/src/App.styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
+  min-height: 100vh;
   font-family: sans-serif;
   text-align: center;
 `
--- average-react-app/src/components/Nav/Nav.js
+++ average-react-app/src/components/Nav/Nav.js
@@ -1,13 +1,34 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { ToggleButton, InnerMenu } from './Nav.styles'
 
 function Nav() {
   const [open, setOpen] = useState(false)
+
+  useEffect(() => {
+    function handleClick(e) {
+      // Chrome uses e.path while Firefox uses e.composedPath()
+      // Returns an array of elements in the path of the click
+      const path = e.path || e.composedPath()
+      const isClickInside = path.some(element => element.id === 'inner-menu')
+      isClickInside ? console.log('clicked inside inner menu') : setOpen(false)
+    }
+
+    document
+      .querySelector('#average-react-app')
+      .addEventListener('click', handleClick, false)
+
+    return () => {
+      document
+        .querySelector('#average-react-app')
+        .removeEventListener('click', handleClick, false)
+    }
+  }, [])
+
   return (
     <header>
       <nav>
         <ToggleButton onClick={() => setOpen(!open)}>Menu</ToggleButton>
-        <InnerMenu open={open}>
+        <InnerMenu open={open} id="inner-menu">
           <ul>
             <li>Home</li>
             <li>Products</li>
