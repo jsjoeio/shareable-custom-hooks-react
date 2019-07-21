7fa0316826426655143a6b041d8ebf78e3890952
JavaScript Joe <jjprevite@gmail.com>
2019-07-21 08:47:06 -0700

    Change a Stateful Class Component to a Function Component with the State Hook
    In this lesson, took a class component which used state and converted it to a function component which utilized the State Hook.

--- average-react-app/src/components/Nav/Nav.js
+++ average-react-app/src/components/Nav/Nav.js
@@ -1,33 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ToggleButton, InnerMenu } from './Nav.styles'
 
-class Nav extends React.Component {
-  state = {
-    open: false
-  }
-
-  render() {
-    return (
-      <header>
-        <nav>
-          <ToggleButton
-            onClick={() => this.setState({ open: !this.state.open })}
-          >
-            Menu
-          </ToggleButton>
-          <InnerMenu open={this.state.open}>
-            <ul>
-              <li>Home</li>
-              <li>Products</li>
-              <li>Values</li>
-              <li>Team</li>
-              <li>Contact</li>
-            </ul>
-          </InnerMenu>
-        </nav>
-      </header>
-    )
-  }
+function Nav() {
+  const [open, setOpen] = useState(false)
+  return (
+    <header>
+      <nav>
+        <ToggleButton onClick={() => setOpen(!open)}>Menu</ToggleButton>
+        <InnerMenu open={open}>
+          <ul>
+            <li>Home</li>
+            <li>Products</li>
+            <li>Values</li>
+            <li>Team</li>
+            <li>Contact</li>
+          </ul>
+        </InnerMenu>
+      </nav>
+    </header>
+  )
 }
 
 export default Nav
