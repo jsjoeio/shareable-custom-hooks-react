d2369c2b63520b7ef373b3bd4352f827524a13cd
JavaScript Joe <jjprevite@gmail.com>
2019-09-01 14:33:12 -0700

    feat: add form submit handler

--- average-react-app/src/components/Feedback/Feedback.class.js
+++ average-react-app/src/components/Feedback/Feedback.class.js
@@ -1,65 +1,16 @@
 import React from 'react'
-import styled from 'styled-components'
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  font-family: Helvetica, Arial, sans-serif;
-  margin: 3rem auto;
-  max-width: 300px;
-  text-align: center;
-`
-
-const Label = styled.label`
-  font-size: 1.25rem;
-`
-
-const Textarea = styled.textarea`
-  border-radius: 20px;
-  border: 2px solid black;
-  box-sizing: border-box;
-  font-family: inherit;
-  height: 150px;
-  margin: 0.5rem auto;
-  padding: 0.75rem;
-  transition: all 0.8s cubic-bezier(0.2, 0.8, 0.2, 1) 0s;
-
-  &:focus,
-  &:active {
-    border: 2px solid #4287f5;
-    background-color: #f2f6fc;
-  }
-`
-
-const Button = styled.button`
-  background-color: black;
-  border: 2px solid black;
-  border-radius: 20px;
-  color: white;
-  cursor: pointer;
-  font-size: 1rem;
-  font-weight: 700;
-  margin: 0.25rem auto;
-  padding: 0.5rem 1rem;
-  transition: all 0.8s cubic-bezier(0.2, 0.8, 0.2, 1) 0s;
-  text-transform: uppercase;
-
-  &:hover,
-  &:focus,
-  &:active {
-    transform: scale(1.1);
-  }
-
-  &:focus {
-    border: 3px solid #4287f5;
-  }
-`
+import { Form, Label, Textarea, Button } from './Feedback.styles'
 
 export class Feedback extends React.Component {
   state = {
     text: ''
   }
 
+  // Handle form submission
+  handleSubmit(e) {
+    e.preventDefault()
+    console.log(`Submitting response to API: "${this.state.text}"`)
+  }
   // Update text in state onchange for textarea
   handleTextChange(e) {
     const updatedText = e.target.value
@@ -71,7 +22,7 @@ export class Feedback extends React.Component {
 
   render() {
     return (
-      <Form>
+      <Form onSubmit={e => this.handleSubmit(e)}>
         <Label>
           Have feedback for our team? <br /> Let us know here 👇
           <Textarea
@@ -79,7 +30,7 @@ export class Feedback extends React.Component {
             onChange={e => this.handleTextChange(e)}
           />
         </Label>
-        <Button>Submit</Button>
+        <Button type="submit">Submit</Button>
       </Form>
     )
   }
