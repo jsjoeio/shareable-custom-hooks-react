f1bafa0b4f3c800a13e84c3160a0641d04ce3dcb
JavaScript Joe <jjprevite@gmail.com>
2019-09-04 19:16:24 -0700

    feat: add custom state hook

---
+++ average-react-app/src/components/Feedback/Feedback.state.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { Form, Label, Textarea, Button } from './Feedback.styles'
+
+function useText(initialState) {
+  return useState(initialState)
+}
+
+export function FeedbackFunctionComponent(props) {
+  const [text, setText] = useText('')
+  useEffect(() => {
+    async function getJoke() {
+      const response = await fetch('https://api.chucknorris.io/jokes/random')
+      const { value } = await response.json()
+      setText(value)
+    }
+    getJoke()
+    console.log('Logging from the feedback function component')
+  }, [])
+
+  // Handle form submission
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log(`Submitting response to API: "${text}"`)
+    setText('')
+  }
+  // Update text in state onchange for textarea
+  function handleTextChange(e) {
+    const updatedText = e.target.value
+    setText(updatedText)
+  }
+  return (
+    <Form onSubmit={e => handleSubmit(e)}>
+      <Label>
+        Have feedback for our team? <br /> Let us know here 👇
+        <Textarea value={text} onChange={e => handleTextChange(e)} />
+      </Label>
+      <Button type="submit">Submit</Button>
+    </Form>
+  )
+}
