fb0e9e0ec14a6e3f126234795573f781800fff7c
JavaScript Joe <jjprevite@gmail.com>
2019-09-07 17:29:41 -0700

    feat: add spec file for tests

---
+++ exercises/testingSpec.js
@@ -0,0 +1,19 @@
+import { renderHook, act } from '@testing-library/react-hooks'
+import { useStarWarsQuote } from './'
+
+describe('useStarWarsQuote', () => {
+  test('should return an object with the keys: loading, error, data', () => {
+    // const { result } = renderHook(() => useStarWarsQuote())
+    // add tests here
+  })
+
+  test('should set loading to true after initial call', () => {
+    // const { result } = renderHook(() => useStarWarsQuote())
+    // add tests here
+  })
+
+  test('should return a joke and set loading to false', async () => {
+    // const { result } = renderHook(() => useStarWarsQuote())
+    // add tests here
+  })
+})
