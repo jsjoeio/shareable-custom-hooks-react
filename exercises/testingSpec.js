26ad970e29379d788dd9b23687ecc661543629c1
JavaScript Joe <jjprevite@gmail.com>
2019-09-14 15:17:10 -0700

    fix: change testingSpec

--- exercises/testingSpec.js
+++ exercises/testingSpec.js
@@ -1,19 +1,34 @@
-import { renderHook, act } from '@testing-library/react-hooks'
+import { renderHook } from '@testing-library/react-hooks'
 import { useStarWarsQuote } from './'
 
 describe('useStarWarsQuote', () => {
-  test('should return an object with the keys: loading, error, data', () => {
-    // const { result } = renderHook(() => useStarWarsQuote())
-    // add tests here
+  test('should return an object with the keys: loading, quote', () => {
+    const { result } = renderHook(() => useStarWarsQuote())
+
+    expect(result.current).toHaveProperty('loading')
+    expect(result.current).toHaveProperty('quote')
   })
 
   test('should set loading to true after initial call', () => {
-    // const { result } = renderHook(() => useStarWarsQuote())
-    // add tests here
+    const { result } = renderHook(() => useStarWarsQuote())
+
+    expect(result.current.loading).toBe(true)
   })
 
-  test('should return a quote and set loading to false', async () => {
-    // const { result } = renderHook(() => useStarWarsQuote())
-    // add tests here
+  test('should return a joke and set loading to false', async () => {
+    const { result, waitForNextUpdate } = renderHook(() => useStarWarsQuote())
+
+    expect(result.current.loading).toBe(true)
+
+    await waitForNextUpdate()
+
+    // By now we should have the quote
+    expect(typeof result.current.quote).toBe('string')
+    // Since it's a random string
+    // We can only make generic assertions
+    expect(result.current.quote).not.toBeNull()
+    expect(result.current.quote).not.toBe('')
+    // And loading should be set to false
+    expect(result.current.loading).toBe(false)
   })
 })
